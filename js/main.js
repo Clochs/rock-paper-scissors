8a599115ee7fc60a9cb24fd60a296bb10d6b3063
Clochs <sam_vs_computer@gmail.com>
2024-07-02 23:10:09 -0500

    Created html, css and js skeleton files and wrote js psuedocode for computer choice

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Create computer choice
+// IF 
+//     random number <= .333
+//     return Rock
+// ELSE IF 
+//     random number <= .666
+//     return Paper
+// ELSE
+//     return Scissors
+
+
+
